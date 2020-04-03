d2f1f195b9fcd91cd8525b55e55bc63fa3378871
playday3008 <titanv3585@gmail.com>
2020-04-03 17:59:08 +0200

    Update info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("DiscordNitroGeneratorBasic")]
+[assembly: AssemblyTitle("DiscordNitroGenerator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DiscordNitroGeneratorBasic")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("DiscordNitroGenerator")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
